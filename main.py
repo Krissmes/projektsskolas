f2869a4571e788f3e307cb63df13f2e4ede2b996
Krissmes <kriss.iesalnieks@gmail.com>
2024-04-02 14:19:36 +0300

    ab

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "wassup!!!"
+    return render_template("index.html")
+
+@app.route("/teksts")
+def teksts():
+      return render_template("teksts.html")
+
+@app.route("/saraksts")
+def saraksts():
+    saraksts = ["Anna", "Katls", "Kartupelis"]
+    bildes = ["","",""]
+    return render_template("saraksts.html", vardi = saraksts)
+
 
 if __name__ == "__main__":
         app.run(port = 5000)
